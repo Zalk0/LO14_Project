a992594201c2a75fb785f7564ea445a14093db47
Gylfirst <matthieu.tourrette@utt.fr>
2022-12-17 22:46:34 +0100

    add features in rsvh function

--- connect.sh
+++ connect.sh
@@ -4,10 +4,28 @@
 
 function rsvh() {
     if [ $# -eq 1 ] && [ "$1" = "-admin" ]; then
-        read -p "What's the pasword for admin?" admin_passwd
-    elif [ $# -eq 3 ] && [ "$1" = "-connect" ];then
-        read -p "What's your password $3?" user_passwd
-    elif [ $# -eq 1 ] && [ "$1" = "-help" ];then
+        read -sp "What's the pasword for admin?" admin_passwd
+        echo -e "\nThe admin password is: $admin_passwd"
+    elif [ $# -eq 3 ] && [ "$1" = "-connect" ]; then
+        file="./machines"
+        compt=0
+        while read -r ligne
+        do
+            machine=$(echo $ligne | cut -f1)
+            if [ "$2" = "$machine" ]; then
+                break
+            else
+                ((compt++))
+            fi
+        done < $file
+        len=$(wc -l $file | cut -d ' ' -f1)
+        if [[ $compt -eq $len ]]; then
+            echo "The specified machine doesn't exist."
+        else
+            read -sp "What's your password $3?" user_passwd
+            echo -e "\nYour password is: $user_passwd"
+        fi
+    elif [ $# -eq 1 ] && [ "$1" = "-help" ]; then
         help
     else
         syntax
@@ -37,3 +55,11 @@ function help() {
     echo -e "wall: allow the admin to send a message to connected users/not connected users following the syntax:\n\twall message (for only connected users)\n\twall -n message (for all users)"
     echo "afinger:"
 }
+
+
+## COMMANDE DE TEST
+# rsvh -admin
+# rsvh -connect machine1 user
+# rsvh -connect machine2 user
+# rsvh -help
+# rsvh dzq
